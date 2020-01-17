4bb25f605b83390502bccfe4d19b4bbb24c7a243
KuriaDan <danielthuku93@gmail.com>
2020-01-17 14:47:30 +0300

    removed keys

--- keys_secret.py
+++ keys_secret.py
@@ -1,5 +1,5 @@
-consumer_key = 'DqexmAvduqe7n591OYcuiXm6g'
-consumer_secret = '4lpiHivoqdCeuXJqjzNXufUdCoVJ0Lk7JRoava3OeWOXKW1EfL'
-access_token = '1105204973208510465-ygmKrngOolMufh3bbEeca29SxodgpP'
-access_token_secret = 'GasW4ojpUD7hhPusu9Ujvgk9KkyPKEXYnejohZg9mfvxa'
+consumer_key = consumer_key
+consumer_secret = consumer secret
+access_token = access_token
+access_token_secret = access token secret
 
